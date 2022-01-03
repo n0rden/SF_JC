4fed508b05ab24d4ef324764a5addc569cde993e
n0rd3n <n0rd3n@mail.ru>
2022-01-03 10:51:19 +0300

    third commit

--- src/Main.java
+++ src/Main.java
@@ -147,7 +147,6 @@ public class Main {
         System.out.println(board.makeMove(2, 2)); // 0
         System.out.println(board.makeMove(3, 1)); // X
         System.out.println(board.makeMove(3, 1)); // X
-        System.out.println(board.makeMove(3, 1)); // X
         System.out.println(board.makeMove(2, 2)); //
 
     }
