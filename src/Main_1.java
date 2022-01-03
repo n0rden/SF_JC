8c0cf1292f908fcd52979aa73090d99dad742ad0
n0rd3n <n0rd3n@mail.ru>
2022-01-03 10:58:16 +0300

    4th commit

--- src/Main_1.java
+++ src/Main_1.java
@@ -1,9 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-public class Main {
+public class Main_1 {
 /*    public static void main(String[] args) {
         int[] a = {1, 1, 1, 2, 1, 3};
         for (int i = 0; i < a.length; i++) {
