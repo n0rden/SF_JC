4768e568ef755713f3b2a57536ea78f6b832e51b
n0rd3n <n0rd3n@mail.ru>
2022-01-08 16:25:40 +0300

    9th commit

---
+++ src/SubClass.java
@@ -0,0 +1,17 @@
+public class SubClass extends SuperClass {
+    SubClass(int a, int b) {
+        super(a);
+        this.b = b;
+    }
+
+    @Override
+    public int add(int v) {
+        return super.add(v) + a;
+    }
+
+    public static void main(String args[]) {
+        SubClass s = new SubClass(10, 12);
+        System.out.println(s.a);
+        System.out.println(s.add(10));
+    }
+}
---
+++ src/SuperClass.java
@@ -0,0 +1,14 @@
+public class SuperClass {
+    public int a;
+    public int b;
+
+    SuperClass(int a) {
+        this.a = 15;
+        this.b = 33;
+    }
+
+    public int add(int v) {
+        this.a += v;
+        return a;
+    }
+}
