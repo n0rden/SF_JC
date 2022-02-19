1407c5d9ec93e74946006fb02b31abb085f1a0f6
n0rd3n <n0rd3n@mail.ru>
2022-02-19 12:44:55 +0300

    Module 9.7 homework

--- src/usr/schedule/Event.java
+++ src/usr/schedule/Event.java
@@ -7,17 +7,15 @@ import java.text.SimpleDateFormat;
 public class Event {
     private String title;
     private Calendar evenStart;
-    // private Calendar evenStop;
     private SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy kk:mm");
 
     public Event(String dateAndTitle) throws Exception {
         evenStart = Calendar.getInstance();
-        // evenStop = Calendar.getInstance();
         int index = dateAndTitle.indexOf(" ");
         title = dateAndTitle.substring(dateAndTitle.indexOf(" ",index + 1) + 1, dateAndTitle.length());
         String dateAndTime = dateAndTitle.substring(0, index);
         Date dateTime = sdf.parse(dateAndTitle);
-        sdf.setCalendar(evenStart);
+        evenStart.setTime(dateTime);
 
         System.out.println(sdf.format(evenStart.getTime()));
     }
