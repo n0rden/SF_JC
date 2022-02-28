f00a8a9e4e2816fc636301bb8db5cf7f396d5e71
n0rd3n <n0rd3n@mail.ru>
2022-02-28 06:35:39 +0300

    Module 11.6 homework

---
+++ src/usr/scheduleV3/Launch.java
@@ -0,0 +1,38 @@
+package usr.scheduleV3;
+
+import java.util.Scanner;
+
+public class Launch {
+
+    static public void start() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        ScheduleV3 schedule = new ScheduleV3();
+
+        System.out.println("\n-= Планировщик задач =-\n\n" +
+                "Введите задачу в формате дата, время и название задачи\n" +
+                "(пример: 23-10-2021 10:00 Стоматолог)");
+
+        help();
+
+        System.out.print("Введите задачу: ");
+        String str = sc.nextLine();
+
+        while (!str.equals("exit")) {
+            if (str.equals("show")) schedule.show();
+            else if (str.equals("save")) schedule.saveToFile();
+            else if (str.equals("help")) help();
+            else schedule.addEvent(new Event(str));
+            str = sc.nextLine();
+        }
+        sc.close();
+    }
+
+    public static void help() {
+        System.out.println("\nКомманды управления программой:\n" +
+                "show - для вывода в консоль расписания\n" +
+                "save - для сохранения в файл (в папку проекта)\n" +
+                "open - для открытия файла расписания (из папки проекта)\n" +
+                "help - для вывода комманд управления программой\n" +
+                "exit - для выхода из программы\n");
+    }
+}
---
+++ src/usr/scheduleV3/ScheduleV3.java
@@ -0,0 +1,58 @@
+package usr.scheduleV3;
+
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class ScheduleV3 {
+
+    Event[] events = new Event[10];
+
+    public void addEvent(Event event) {
+        int arrLength = events.length;
+
+        if (events[arrLength - 1] != null) {
+            System.out.println("Расписание на день полное");
+        } else {
+            for (int i = 0; i < arrLength; i++) {
+                if (events[i] == null) {
+                    events[i] = event;
+                    System.out.println("Задача добавлена в расписание");
+                    break;
+                }
+            }
+        }
+    }
+
+    public void saveToFile() throws Exception {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd_MM_yyyy_kk_mm");
+        FileOutputStream fos = new FileOutputStream("Schedule_" + sdf.format(calendar.getTime()) + ".txt");
+
+        if (events[0] == null) {
+            System.out.println("Расписание пустое, сохранять нечего");
+        } else {
+            for (Event event : events) {
+                if (event != null) {
+                    fos.write(event.toString().getBytes(StandardCharsets.UTF_8));
+                }
+            }
+            fos.close();
+            System.out.println("Данные записаны в файл");
+        }
+    }
+
+    public void show() {
+
+        if (events[0] == null) {
+            System.out.println("Расписание пустое");
+        } else {
+            for (Event event : events) {
+                if (event != null) {
+                    System.out.println(event);
+                }
+            }
+        }
+    }
+}
