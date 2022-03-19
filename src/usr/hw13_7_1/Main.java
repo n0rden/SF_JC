d10f3cd0831ea31c92e5c5510480cc40f3d0f235
n0rd3n <n0rd3n@mail.ru>
2022-03-19 22:47:15 +0300

    Module 12.8 homework Replaced the array in the "ScheduleV3" class with TreeSet

---
+++ src/usr/hw13_7_1/Main.java
@@ -0,0 +1,22 @@
+package usr.hw13_7_1;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        int[] intsArr1 = { 14, 34, 34, 8, 9, 152, 7 };
+        int[] intsArr2 = { 1, 13, 7, 669, 3 };
+        int[] intsArr3 = { 79, 71, 9, 11, 4, 23 };
+
+        // создаем 3 потока
+        SaveAsThread saveAsThread1 = new SaveAsThread(intsArr1, "saveAsThread1.txt", "thread1");
+        SaveAsThread saveAsThread2 = new SaveAsThread(intsArr2, "saveAsThread2.txt", "thread2");
+        SaveAsThread saveAsThread3 = new SaveAsThread(intsArr3, "saveAsThread3.txt", "thread3");
+
+        saveAsThread1.start();
+        saveAsThread2.start();
+        saveAsThread3.start();
+    }
+
+}
---
+++ src/usr/hw13_7_1/SaveAsThread.java
@@ -0,0 +1,53 @@
+package usr.hw13_7_1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class SaveAsThread implements Runnable {
+    private int[] intsArr;      // записываемый массив
+    private String filename;    // им€ файла
+    private String threadName;  // им€ потока
+    private Thread thread;      // ссылка на текущий поток
+
+    //  онструктор - получает 3 параметра:
+    // intsArr - массив, который нужно записать в файл;
+    // filename - им€ файла, в который записываетс€ массив AI;
+    // threadName - им€ потока.
+    public SaveAsThread(int[] intsArr, String filename, String threadName) {
+        this.intsArr = intsArr;
+        this.filename = filename;
+        this.threadName = threadName;
+
+        thread = new Thread(this, "SaveThread");
+    }
+
+    // запускаем текущий поток
+    public void start() {
+        thread.start();
+    }
+
+    public void run() {
+        System.out.println("Begin thread: " + threadName);
+
+        try {
+            FileOutputStream fos = new FileOutputStream(filename);
+            PrintStream printStream = new PrintStream(fos);
+
+            // «аписываем массив intsArr в файл
+            printStream.println(intsArr.length);
+            for (int i = 0; i < intsArr.length; i++) {
+                printStream.println(intsArr[i]);
+            }
+
+            printStream.close();
+            fos.close();
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        // —ообщаем о завершении потока
+        System.out.println("End thread: " + threadName);
+    }
+}
+
