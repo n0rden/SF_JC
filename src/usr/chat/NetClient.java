846da21f1511c3881a3bfcb76853ac978c3c2f4c
Andrew <andrew_lex@mail.ru>
2022-03-19 11:40:34 +0300

    Homework 13.3.3 Chat

--- src/usr/chat/NetClient.java
+++ src/usr/chat/NetClient.java
@@ -15,86 +15,88 @@ import javax.swing.JTextArea;
 
 public class NetClient extends JFrame implements KeyListener {
 
-	final String serverIP = "127.0.0.1";
-	final int serverPort = 1234;
+    final String serverIP = "127.0.0.1";
+    final int serverPort = 1234;
 
-	JTextArea textArea;
-	JScrollPane scrollPane;
-	InputStreamReader in;
-	PrintWriter out;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    InputStreamReader in;
+    PrintWriter out;
 
-	NetClient() {
-		// Создаем окно
-		super("Simple Chat client");
-		setSize(400, 500);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
+    NetClient() {
+        // Создаем окно
+        super("Simple Chat client");
+        setSize(400, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		// Добавляем на окно текстовое поле
-		textArea = new JTextArea();
-		textArea.setBackground(Color.BLACK);
-		textArea.setForeground(Color.WHITE);
-		textArea.setEditable(false);
-		textArea.setMargin(new Insets(10, 10, 10, 10));
-		scrollPane = new JScrollPane(textArea);
-		this.add(scrollPane);
+        // Добавляем на окно текстовое поле
+        textArea = new JTextArea();
+        textArea.setBackground(Color.BLACK);
+        textArea.setForeground(Color.WHITE);
+        textArea.setEditable(false);
+        textArea.setMargin(new Insets(10, 10, 10, 10));
+        scrollPane = new JScrollPane(textArea);
+        this.add(scrollPane);
 
-		// Подсоединяемся к серверу
-		connect();
+        // Подсоединяемся к серверу
+        connect();
+    }
 
-	}
+    void connect() {
+        try {
+            Socket socket = new Socket(serverIP, serverPort);
+            in = new InputStreamReader(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream());
+            textArea.addKeyListener(this);
+        } catch (IOException e) {
+            textArea.setForeground(Color.RED);
+            textArea.append("Server " + serverIP + " port " + serverPort + " " + "" + "NOT AVAILABLE");
+            e.printStackTrace();
+        }
+        new Thread() {
+            // в отдельном потоке
+            // принимаем символы от сервера
+            public void run() {
+                while (true) {
+                    try {
+                        addCharToTextArea((char) (in.read()));
+                    } catch (IOException e) {
+                        textArea.setForeground(Color.RED);
+                        textArea.append("\nCONNECTION ERROR");
+                        e.printStackTrace();
+                        return;
+                    }
+                }
+            }
 
-	void connect() {
-		try {
-			Socket socket = new Socket(serverIP, serverPort);
-			in = new InputStreamReader(socket.getInputStream());
-			out = new PrintWriter(socket.getOutputStream());
-			textArea.addKeyListener(this);
-		} catch (IOException e) {
-			textArea.setForeground(Color.RED);
-			textArea.append("Server " + serverIP + " port " + serverPort + " " + "" + "NOT AVAILABLE");
-			e.printStackTrace();
-		}
-		new Thread() {
-			// в отдельном потоке
-			// принимаем символы от сервера
-			public void run() {
-				while (true) {
-					try {
-						addCharToTextArea((char) (in.read()));
-					} catch (IOException e) {
-						textArea.setForeground(Color.RED);
-						textArea.append("\nCONNECTION ERROR");
-						e.printStackTrace();
-						return;
-					}
-				}
-			};
-		}.start();
+            ;
+        }.start();
+    }
 
-	}
+    public static void main(String[] args) {
+        new NetClient().setVisible(true);
+    }
 
-	public static void main(String[] args) {
-		new NetClient().setVisible(true);
-	}
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+    }
 
-	@Override
-	public void keyPressed(KeyEvent arg0) {}
-	@Override 
-	public void keyReleased(KeyEvent arg0) {}
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+    }
 
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// отправляем напечатанный символ в сеть и на экран
-		out.print(arg0.getKeyChar());
-		out.flush();
-		
-		System.out.print((int)(arg0.getKeyChar()));
-		addCharToTextArea(arg0.getKeyChar());
-	}
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+        // отправляем напечатанный символ в сеть и на экран
+        out.print(arg0.getKeyChar());
+        out.flush();
 
-	void addCharToTextArea(char c) {
-		textArea.append(c + "");
-		textArea.setCaretPosition(textArea.getDocument().getLength());
-	}
+        System.out.print((int) (arg0.getKeyChar()));
+        addCharToTextArea(arg0.getKeyChar());
+    }
 
+    void addCharToTextArea(char c) {
+        textArea.append(c + "");
+        textArea.setCaretPosition(textArea.getDocument().getLength());
+    }
 }
