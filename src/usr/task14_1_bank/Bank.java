586bc18282f5ee99fab2a9c0a2f3a9e65b545d0f
n0rd3n <n0rd3n@mail.ru>
2022-03-20 13:43:30 +0300

    Module 14

--- src/usr/task14_1_bank/Bank.java
+++ src/usr/task14_1_bank/Bank.java
@@ -4,24 +4,21 @@ public class Bank {
 
     private static int money = 10000;
 
-    public Bank() {
-        new Client().start();
-        new Client().start();
-        new Client().start();
-    }
-
     public static synchronized void setMoney(int money) {
         Bank.money += money;
     }
 
-    public int getMoney() {
+    public static int getMoney() {
         return money;
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Bank bank = new Bank();
-        while(true) {
-            System.out.println(bank.getMoney());
+        new Client().start();
+        new Client().start();
+        new Client().start();
+
+        while (true) {
+            System.out.println(Bank.getMoney());
             Thread.sleep(1000);
         }
     }
